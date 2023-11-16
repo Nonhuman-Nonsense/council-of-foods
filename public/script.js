b59520bb3637b5b72a69e6806fd102c2734289ac
Elliott Cost <email@elliott.computer>
2023-11-16 15:46:58 +0200

    first commit

---
+++ public/script.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const socket = io();
+
+    // Adding a character to the panel
+    const addCharacterBtn = document.getElementById('add-character');
+    addCharacterBtn.addEventListener('click', () => {
+        const characters = document.getElementById('characters');
+        const characterCount = characters.getElementsByClassName('character').length;
+
+        if (characterCount < 10) {
+            const newCharacterDiv = document.createElement('div');
+            newCharacterDiv.classList.add('character');
+            newCharacterDiv.innerHTML = `
+                <input type="text" placeholder="character name"> 
+                <input type="text" placeholder="character role">
+            `;
+            characters.appendChild(newCharacterDiv);
+        } else {
+            alert('Maximum of 10 characters reached');
+        }
+    });
+
+    // Start the conversation
+    document.getElementById('startBtn').addEventListener('click', () => {
+        const characters = document.querySelectorAll('#characters .character');
+        const characterData = Array.from(characters).map(characterDiv => {
+            const inputs = characterDiv.querySelectorAll('input');
+            return {
+                name: inputs[0].value,
+                role: inputs[1].value
+            };
+        });
+
+        // Retrieve the topic from the panel-prompt element
+        const topic = document.getElementById('panel-prompt').value;
+        console.log(topic);
+        console.log(characterData);
+
+        socket.emit('start_conversation', { characterData, topic });
+
+        // Listening to conversation updates
+        socket.on('conversation_update', (conversation) => {
+            const conversationDiv = document.getElementById('conversation');
+            conversationDiv.innerHTML = conversation
+                .map(turn => `<p><strong>${turn.speaker}:</strong> ${turn.text}</p>`)
+                .join('');
+        });
+
+        // Handle conversation end
+        socket.on('conversation_end', () => {
+            alert('Conversation has ended!');
+        });
+
+        // Handle errors
+        socket.on('conversation_error', (errorMessage) => {
+            console.error(errorMessage);
+            // Handle errors appropriately
+        });
+    });
+});
---
+++ server.js
@@ -0,0 +1,127 @@
+require('dotenv').config();
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+const OpenAI = require('openai');
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+const maxResponseLength = 100; // Maximum response length
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    let conversation = [];
+    let characterRoles = {};
+
+    socket.on('start_conversation', ({ characterData, topic }) => {
+        console.log(`Conversation started on topic: ${topic}`);
+        console.log('Character Data:', characterData);
+        
+        // Resetting conversation and characterRoles for new conversation
+        conversation = [];
+        characterRoles = {};
+    
+        // Check if characterData is valid
+        if (!Array.isArray(characterData) || characterData.length === 0) {
+            console.error('Invalid character data received.');
+            socket.emit('conversation_error', 'Invalid character data.');
+            return;
+        }
+    
+        // Process each character
+        characterData.forEach(char => {
+            characterRoles[char.name.trim()] = char.role.trim();
+        });
+    
+        if (Object.keys(characterRoles).length === 0) {
+            console.error('No valid characters found.');
+            socket.emit('conversation_error', 'No valid characters for conversation.');
+            return;
+        }
+    
+        // Determine the first speaker
+        const firstSpeaker = Object.keys(characterRoles)[0];
+        console.log('First Speaker:', firstSpeaker);
+    
+        // Create the first prompt
+        const firstPrompt = { speaker: firstSpeaker, text: `${firstSpeaker}, ${topic}` };
+        handleConversationTurn(firstPrompt, socket, conversation, topic, characterRoles);
+    });    
+
+    const handleConversationTurn = async (prompt, socket, conversation, topic, characterRoles) => {
+        try {
+            const response = await generateTextFromGPT4(prompt.text, prompt.speaker);
+            conversation.push({ speaker: prompt.speaker, text: response });
+
+            socket.emit('conversation_update', conversation);
+            if (conversation.length >= 10) {
+            socket.emit('conversation_end', conversation);
+            return;
+            }
+
+            const nextSpeaker = determineNextSpeaker(conversation);
+            const role = characterRoles[nextSpeaker] || 'Participant'; // Default role if not found
+            const nextPromptText = `${conversation.map(turn => turn.text).join(' ')} ${role} ${nextSpeaker}, ${topic}`;
+            const nextPrompt = { speaker: nextSpeaker, text: nextPromptText };
+
+            handleConversationTurn(nextPrompt, socket, conversation, topic, characterRoles);
+        } catch (error) {
+            console.error('Error during conversation:', error);
+            socket.emit('conversation_error', 'An error occurred during the conversation.');
+        }
+    };
+
+    const generateTextFromGPT4 = async (prompt, speaker) => {
+        try {
+        const roleIntroduction = characterRoles[speaker] ? `I am ${characterRoles[speaker]}. ` : '';
+        const promptWithRole = roleIntroduction + prompt;
+    
+        const completion = await openai.chat.completions.create({
+            messages: [
+            {"role": "system", "content": `You are currently role-playing as a ${speaker} on a panel discussion.`},
+            {"role": "user", "content": promptWithRole}
+            ],
+            model: "gpt-3.5-turbo",
+            max_tokens: maxResponseLength
+        });
+    
+        let response = completion.choices[0].message.content.trim();
+    
+        // Trim to the last complete sentence to avoid cutting off mid-sentence
+        response = response.replace(/\s+$/, ''); // Remove trailing whitespace
+        const lastPeriodIndex = response.lastIndexOf('.');
+        if (lastPeriodIndex !== -1) {
+            response = response.substring(0, lastPeriodIndex + 1);
+        }
+    
+        return response;
+        } catch (error) {
+        console.error('Error during API call:', error);
+        throw error;
+        }
+    };
+    
+
+    const determineNextSpeaker = (conversation) => {
+        const speakers = Object.keys(characterRoles);
+        const lastSpeaker = conversation.length > 0 ? conversation[conversation.length - 1].speaker : null;
+        const lastSpeakerIndex = speakers.indexOf(lastSpeaker);
+
+        const nextSpeakerIndex = lastSpeakerIndex === -1 ? 0 : (lastSpeakerIndex + 1) % speakers.length;
+        return speakers[nextSpeakerIndex];
+    }; 
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+httpServer.listen(3000, () => {
+    console.log('Listening on *:3000');
+});
