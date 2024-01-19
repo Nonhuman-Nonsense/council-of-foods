54fa259579534c3fd057231a248b474741c2652b
nonhumannonsense <hello@nonhuman-nonsense.com>
2024-01-19 22:29:30 +0100

    refactored many things: api calling, paus button, restart, keep going, human input fixed etc

--- public/script.js
+++ public/script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const newCharacterDiv = document.createElement('div');
             newCharacterDiv.classList.add('character');
             newCharacterDiv.innerHTML = `
-                <input type="text" placeholder="character name"> 
+                <input type="text" placeholder="character name">
                 <textarea id="character-role" placeholder="character role"></textarea>
             `;
             characters.appendChild(newCharacterDiv);
@@ -20,107 +20,205 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    document.getElementById('factoryResetButton').addEventListener('click', () => {
+      localStorage.clear();
+      location.reload();
+    });
+
+
     let conversationActive = false;
     let conversationStarted = false;
+    let promptsAndOptions;
+
+    const unpackPromptsAndOptions = () => {
+      console.log(promptsAndOptions);
+      //Update the UI with the data stored in localStorage
+      //Do the same as update, but reverse
+      document.getElementById('max-response-char-count').value = promptsAndOptions.options.maxResponseCharCountInput;
+      document.getElementById('trim-response-to-full-sentance').checked = promptsAndOptions.options.trimSentance;
+      document.getElementById('conversation-max-length').value = promptsAndOptions.options.conversationMaxLength;
+      document.getElementById('gpt-model').value = promptsAndOptions.options.gptModel;
+
+      // Retrieve the panel topic
+      document.getElementById('panel-prompt').value = promptsAndOptions.topic;
+
+      // Build array of characters
+      let characterDiv = document.createElement("div");
+      characterDiv.id = "characters";
+      promptsAndOptions.characters.forEach((character) => {
+        const newDiv = document.createElement("div");
+        newDiv.className = "character";
+        const inputDiv = document.createElement("input");
+        inputDiv.id = "character-name";
+        inputDiv.placeholder = "character name";
+        inputDiv.type = "text";
+        inputDiv.value = character.name;
+        const textDiv = document.createElement("textarea");
+        textDiv.id = "character-role";
+        textDiv.placeholder = "character role";
+        textDiv.value = character.role;
+        newDiv.appendChild(inputDiv);
+        newDiv.appendChild(textDiv);
+        characterDiv.appendChild(newDiv);
+      });
+      document.getElementById('characters').replaceWith(characterDiv);
+    }
+
+    // This is the global object containing all the prompts and options
+    // It is sent to the server on conversation start, and resume.
+    // Default types are added here just to describe data type
+    try{
+      promptsAndOptions = JSON.parse(localStorage.getItem("PromptsAndOptions"));
+      unpackPromptsAndOptions();
+    }catch(e){
+      console.log(e);
+      console.log('Resetting to default settings');
+      promptsAndOptions = {
+        topic: "",
+        characters: {},
+        options: {}
+      };
+    }
 
     const toggleConversationBtn = document.getElementById('toggleConversationBtn');
+    const restartBtn = document.getElementById('restartButton');
+    const continueBtn = document.getElementById('continueButton');
+    const conversationDiv = document.getElementById('conversation');
+    const endMessage = document.getElementById('end-message');
+    const spinner  = document.getElementById('spinner');
 
     toggleConversationBtn.addEventListener('click', () => {
         if (conversationActive) {
-            socket.emit('pause_conversation');
-            toggleConversationBtn.textContent = 'Start';
+          //Pause the conversation
+            toggleConversationBtn.textContent = 'Resume';
+            restartBtn.style.display = 'inline';
             conversationActive = false;
-
-            document.querySelector('.loader').style.display = 'none';
+            spinner.style.display = 'none';
+            socket.emit('pause_conversation');
         } else {
-            document.querySelector('.loader').style.display = 'block';
+          //Start from scratch or resume
+          spinner.style.display = 'block';
+          continueBtn.style.display = 'none';
+          restartBtn.style.display = 'none';
+          toggleConversationBtn.textContent = 'Pause';
+          conversationActive = true;
 
-            if (!conversationStarted) {
-                startConversation();
-            } else {
-                // Resume the conversation if it's paused
-                isPaused = false; 
-                console.log('Conversation has been resumed');
-            }
-            toggleConversationBtn.textContent = 'Pause';
-            conversationActive = true;
+          if (!conversationStarted) {
+            //Start the conversation from scratch!
+            spinner.style.display = 'block';
+            updatePromptsAndOptions();
+            // Emit the start conversation event with all necessary data
+            conversationStarted = true;
+            socket.emit('start_conversation', promptsAndOptions);
+          } else {
+            // Resume the conversation if it's paused
+            isPaused = false;
+            console.log('Conversation has been resumed');
+            updatePromptsAndOptions();
+            socket.emit('resume_conversation', promptsAndOptions);
+          }
         }
     });
-    
 
-    const startConversation = () => {
-        // Show the spinner or some loading indication
-        document.getElementById('spinner').style.display = 'block';
-    
-        // Gather character data
-        const characters = document.querySelectorAll('#characters .character');
-        const characterData = Array.from(characters).map(characterDiv => {
-            const nameInput = characterDiv.querySelector('input').value; // Assuming the first input is still for the name
-            const roleTextarea = characterDiv.querySelector('textarea').value; // Select the textarea for the role
-        
-            return {
-                name: nameInput,
-                role: roleTextarea
-            };
-        });
-    
-        // Gather chairperson data
-        const chairpersonName = document.getElementById('character-name').value;
-        const chairpersonRole = document.getElementById('character-role').value;
-        const chairpersonData = {
-            name: chairpersonName,
-            role: chairpersonRole
-        };
-    
-        // Retrieve the panel topic
-        const topic = document.getElementById('panel-prompt').value;
-    
-        // Retrieve the frequency of chairperson interjections
-        const chairmanFreq = document.querySelector('#chairman-freq').value;
+    restartBtn.addEventListener('click', () => {
+      //Continue to generate more even after it has previously stopped
+      spinner.style.display = 'block';
+      continueBtn.style.display = 'none';
+      restartBtn.style.display = 'none';
+      toggleConversationBtn.style.display = 'inline';
+      toggleConversationBtn.textContent = 'Pause';
+      conversationActive = true;
+      endMessage.innerHTML = "";
+      conversationDiv.innerHTML = "";
+      updatePromptsAndOptions();
+      // Emit the start conversation event with all necessary data
+      socket.emit('start_conversation', promptsAndOptions);
+    });
 
-        const maxResponseCharCountInput = document.querySelector('#max-response-char-count').value;
-    
-        // Emit the start conversation event with all necessary data
-        socket.emit('start_conversation', { characterData, topic, chairpersonData, chairmanFreq, maxResponseCharCountInput });
-    
-        // Handle conversation updates
-        socket.on('conversation_update', (conversation) => {
-            const conversationDiv = document.getElementById('conversation');
-            conversationDiv.innerHTML = conversation
-                .map(turn => `<p><strong>${turn.speaker}:</strong> ${turn.text}</p>`)
-                .join('');
-        });
-    
-        // Handle conversation end
-        socket.on('conversation_end', () => {
-            document.getElementById('spinner').style.display = 'none';
-        });
-    
-        // Handle conversation error
-        socket.on('conversation_error', (errorMessage) => {
-            console.error(errorMessage);
-            document.getElementById('spinner').style.display = 'none';
-        });
+    continueBtn.addEventListener('click', () => {
+      //Continue to generate more even after it has previously stopped
+      spinner.style.display = 'block';
+      restartBtn.style.display = 'none';
+      continueBtn.style.display = 'none';
+      toggleConversationBtn.style.display = 'inline';
+      toggleConversationBtn.textContent = 'Pause';
+      conversationActive = true;
+      endMessage.innerHTML = "";
+      updatePromptsAndOptions();
+      // Emit the start conversation event with all necessary data
+      socket.emit('continue_conversation', promptsAndOptions);
+    });
 
-        conversationStarted = true;
-    };    
-        
-    
+    const updatePromptsAndOptions = () => {
+      // Retrieve the global options
+
+      promptsAndOptions.options.maxResponseCharCountInput = +document.getElementById('max-response-char-count').value;
+      promptsAndOptions.options.trimSentance = document.getElementById('trim-response-to-full-sentance').checked;
+      promptsAndOptions.options.conversationMaxLength = +document.getElementById('conversation-max-length').value;
+      promptsAndOptions.options.gptModel = document.getElementById('gpt-model').value;
+
+      // Retrieve the panel topic
+      promptsAndOptions.topic = document.getElementById('panel-prompt').value;
+
+      // Gather character data
+      const characters = document.querySelectorAll('#characters .character');
+
+      promptsAndOptions.characters = Array.from(characters).map(characterDiv => {
+          const nameInput = characterDiv.querySelector('input').value; // Assuming the first input is still for the name
+          const roleTextarea = characterDiv.querySelector('textarea').value; // Select the textarea for the role
+
+          return {
+              name: nameInput,
+              role: roleTextarea
+          };
+      });
+
+      localStorage.setItem("PromptsAndOptions", JSON.stringify(promptsAndOptions));
+    }
+
+
+    // Handle conversation updates
+    socket.on('conversation_update', (conversation) => {
+        conversationDiv.innerHTML = conversation
+            .map(turn => `<p><strong>${turn.speaker}:</strong> ${turn.text.split('\n').join('<br>')}</p>`)
+            .join('');
+    });
+
+    // Handle conversation end
+    socket.on('conversation_end', () => {
+        spinner.style.display = 'none';
+        toggleConversationBtn.style.display = 'none';
+        restartBtn.style.display = 'inline';
+        continueBtn.style.display = 'inline';
+        endMessage.innerHTML = "End of Conversation";
+        conversationActive = false;
+    });
+
+    // Handle conversation error
+    socket.on('conversation_error', (errorMessage) => {
+        console.error(errorMessage);
+        spinner.style.display = 'none';
+    });
+
+
     // Handle submission of human input
     document.getElementById('submitHumanInput').addEventListener('click', () => {
-        let humanMessage = document.getElementById('humanInput').value;
+      const message = {
+        speaker: document.getElementById('human-name').value,
+        text: document.getElementById('humanInput').value
+      }
 
         if (conversationActive == false) {
-            socket.emit('submit_human_message', humanMessage);
-            toggleConversationBtn.textContent = 'Pause';
-            conversationActive = true;
-
-            document.querySelector('.loader').style.display = 'block';
+          socket.emit('submit_human_message', message);
+          toggleConversationBtn.textContent = 'Pause';
+          conversationActive = true;
+          spinner.style.display = 'block';
+          toggleConversationBtn.style.display = 'inline';
+          restartBtn.style.display = 'none';
+          continueBtn.style.display = 'none';
         } else {
-            socket.emit('submit_human_message', humanMessage);
+            socket.emit('submit_human_message', message);
         }
     });
-    
-    
 
 });
--- server.js
+++ server.js
@@ -15,18 +15,22 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY);
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
+
+    //Set up the variables accessible to this socket
+    //These can be accessed within any function
     console.log('A user connected');
-    let conversation = [];
-    let characterRoles = {};
-    let chairperson = {};
-    let chairmanFreq = 5; // Default frequency
-    let conversationCount = 0;
-    let topic;
-    let maxResponseCharCount = 100;
 
+    let run = true; //Flag to cancel recursive api calls if socket is closed, ie. disconnected etc.
     let isPaused = false; // Flag to check if the conversation is paused
+    let conversation = [];
+    let conversationCount = 0;
+    let currentSpeaker = 0;
+    let extraMessageCount = 0;
 
-    let lastPrompt;
+    //These are updated on conversation start and resume
+    let topic = "";
+    let characters = {};
+    let options = {};
 
     socket.on('pause_conversation', () => {
         isPaused = true;
@@ -34,184 +38,140 @@ io.on('connection', (socket) => {
     });
 
     // Add a new event to resume the conversation
-    socket.on('resume_conversation', () => {
+    socket.on('resume_conversation', (promptsAndOptions) => {
+        console.log('Conversation has been resumed');
         isPaused = false;
-        console.log('Conversation has been resumed');
-        console.log('Conversation has been resumed');
-        if (lastPrompt) {
-            handleConversationTurn(lastPrompt, socket, conversation, topic, characterRoles, chairmanFreq, maxResponseCharCount);
-        }
+        parsePromptsAndOptions(promptsAndOptions);
+        handleConversationTurn();
     });
 
     socket.on('submit_human_message', (message) => {
+      console.log("A human message is received!");
         if (isPaused) {
             // Resume the conversation
             isPaused = false;
-            console.log('Conversation has been resumed');
-    
-            // Now process the human message
-            const humanPrompt = { speaker: 'Human', text: message };
-            handleConversationTurn(humanPrompt, socket, conversation, topic, characterRoles, chairmanFreq, maxResponseCharCount);
-        } else {
-            // If the conversation is not paused, just process the message
-            console.log('Message received:', message);
-            const humanPrompt = { speaker: 'Human', text: message };
-            handleConversationTurn(humanPrompt, socket, conversation, topic, characterRoles, chairmanFreq, maxResponseCharCount);
+            // Only needs to be started if it's not running
+            handleConversationTurn();
         }
+        //Just inject the message into the conversation stack, and let everyone deal with it!
+        conversation.push(message);
     });
-    
 
-    socket.on('start_conversation', ({ characterData, topic, chairpersonData, chairmanFreq, maxResponseCharCountInput }) => {
-        // console.log(`Conversation started on topic: ${topic}`);
-        
-        conversation = [];
-        characterRoles = {};
-        chairperson = chairpersonData;
-        conversationCount = 0;
-        chairmanFreq = chairmanFreq;
-        maxResponseCharCount = +maxResponseCharCountInput;
+    socket.on('continue_conversation', (promptsAndOptions) => {
+      console.log('Conversation has been continued');
+      parsePromptsAndOptions(promptsAndOptions);
+      extraMessageCount += options.conversationMaxLength;
+      isPaused = false;
+      // Determine the next speaker
+      currentSpeaker = currentSpeaker >= characters.length - 1 ? 0 : currentSpeaker+1;
+      // Start with the chairperson introducing the topic
+      handleConversationTurn();
+    });
 
-        console.log(maxResponseCharCount);
-    
-        characterData.forEach(char => {
-            characterRoles[char.name.trim()] = char.role.trim();
-        });
+    socket.on('start_conversation', (promptsAndOptions) => {
+      parsePromptsAndOptions(promptsAndOptions);
+      conversation = [];
+      conversationCount = 0;
+      currentSpeaker = 0;
+      extraMessageCount = 0;
+      isPaused = false;
 
-        // Add chairperson to character roles
-        if (chairperson && chairperson.name) {
-            characterRoles[chairperson.name] = chairperson.role;
-        }
-    
-        if (Object.keys(characterRoles).length === 0) {
-            console.error('No valid characters found.');
-            socket.emit('conversation_error', 'No valid characters for conversation.');
-            return;
-        }
+      // Start with the chairperson introducing the topic
+      handleConversationTurn();
+    });
 
-        // console.log(characterData);
-    
-        // Start with the chairperson introducing the topic
-        const firstPrompt = { speaker: chairperson.name, text: topic };
-        // console.log(firstPrompt);
-        handleConversationTurn(firstPrompt, socket, conversation, topic, characterRoles, chairmanFreq, maxResponseCharCount);
-    }); 
-
-    const handleConversationTurn = async (prompt, socket, conversation, topic, characterRoles, chairmanFreq, maxResponseCharCount) => {
+    const handleConversationTurn = async () => {
         try {
+            if(!run) return;
             if (isPaused) return; // Don't proceed if the conversation is paused
-    
+
             let response;
-    
-            if (prompt.speaker === 'Human') {
-                // Directly use the human's message
-                response = prompt.text;
-            } else {
-                // Generate response using GPT-4 for AI characters
-                response = await generateTextFromGPT4(topic, prompt.speaker, prompt.text, maxResponseCharCount);
-                console.log(prompt);
-            }
-    
+
+            // Generate response using GPT-4 for AI characters
+            response = await generateTextFromGPT(characters[currentSpeaker]);
+
             // Add the response to the conversation
-            conversation.push({ speaker: prompt.speaker, text: response });
+            conversation.push({ speaker: characters[currentSpeaker].name, text: response });
             socket.emit('conversation_update', conversation);
-    
+
             // Check for conversation end
-            if (conversation.length >= 100) {
+            if (conversation.length >= options.conversationMaxLength + extraMessageCount) {
                 socket.emit('conversation_end', conversation);
                 return;
             }
-    
-            // Determine the next speaker
-            let nextSpeaker;
-            if ((conversation.length + 1) % chairmanFreq === 0) { // Chairman speaks every three messages
-                nextSpeaker = chairperson.name;
-            } else {
-                nextSpeaker = determineNextSpeaker();
-            }
-    
-            // Prepare the next prompt
-            const role = characterRoles[nextSpeaker] || 'Participant';
-            const nextPromptText = `${role} ${nextSpeaker}`;
-            const nextPrompt = { speaker: nextSpeaker, text: nextPromptText };
 
-            // Update lastPrompt with the current prompt before calling the function again
-            lastPrompt = nextPrompt;
-    
-            handleConversationTurn(nextPrompt, socket, conversation, topic, characterRoles, chairmanFreq, maxResponseCharCount);
-            // Increment conversationCount only if it's not the chairman speaking
-            if (nextSpeaker !== chairperson.name) {
-                conversationCount++;
-            }
+            // Determine the next speaker
+            currentSpeaker = currentSpeaker >= characters.length - 1 ? 0 : currentSpeaker+1;
+
+            handleConversationTurn();
         } catch (error) {
             console.error('Error during conversation:', error);
             socket.emit('conversation_error', 'An error occurred during the conversation.');
         }
     };
 
-    const generateTextFromGPT4 = async (topic, speaker, speakerPrompt, maxResponseCharCount) => {
+    const generateTextFromGPT = async (speaker) => {
         try {
             // Build the array of messages for the completion request
             const messages = [];
-    
+
             // System message for overall context
             messages.push({
                 role: "system",
-                content: `You are currently role-playing as a ${speaker} in a panel discussion. The topic is: ${topic}.`
+                content: `${topic}\n\n${speaker.role}`
             });
-    
-            // Assistant message for the current speaker's prompt
-            messages.push({
-                role: "assistant",
-                content: speakerPrompt
-            });
-    
+
             // Add previous messages as separate user objects
             conversation.forEach((msg) => {
                 messages.push({
-                    role: "user",
-                    content: 'This is a previous message in the conversation. Just use them as a reference: ' + msg.text
+                    role: (speaker.name == msg.speaker ? "assistant" : "user"),
+                    content: msg.text
                 });
             });
-            
-            console.log(maxResponseCharCount);
 
             // Prepare the completion request
+            // console.log(conversation.length);
+            // console.log(messages);
             const completion = await openai.chat.completions.create({
-                model: "gpt-4",
-                max_tokens: maxResponseCharCount,
+                model: options.gptModel,
+                max_tokens: options.maxResponseCharCountInput,
                 messages: messages
             });
-    
+
             // Extract and clean up the response
             let response = completion.choices[0].message.content.trim();
-            response = response.replace(/\s+$/, '');
-            const lastPeriodIndex = response.lastIndexOf('.');
-            if (lastPeriodIndex !== -1) {
-                response = response.substring(0, lastPeriodIndex + 1);
+
+            //Remove the last half sentence
+            if(options.trimSentance){
+              response = response.replace(/\s+$/, '');
+              const lastPeriodIndex = response.lastIndexOf('.');
+              if (lastPeriodIndex !== -1) {
+                  response = response.substring(0, lastPeriodIndex + 1);
+              }
             }
-    
             return response;
         } catch (error) {
             console.error('Error during API call:', error);
             throw error;
         }
     };
-    
-    
-    
- 
 
-    const determineNextSpeaker = () => {
-        const speakers = Object.keys(characterRoles).filter(name => name !== chairperson.name);
-        const lastSpeaker = conversation.length > 0 ? conversation[conversation.length - 1].speaker : null;
-        const lastSpeakerIndex = speakers.indexOf(lastSpeaker);
+    const parsePromptsAndOptions = (p) => {
+      topic = p.topic;
+      characters = p.characters;
+      options = p.options;
 
-        const nextSpeakerIndex = lastSpeakerIndex === -1 ? 0 : (lastSpeakerIndex + 1) % speakers.length;
-        return speakers[nextSpeakerIndex];
-    }; 
+      if (Object.keys(characters).length === 0) {
+        console.error('No valid characters found.');
+        socket.emit('conversation_error', 'No valid characters for conversation.');
+        return;
+      }
+    }
 
     socket.on('disconnect', () => {
+        run = false;
         console.log('User disconnected');
+        //After this, the socket and all variables will be garbage collected automatically, since all loops stop
     });
 });
 
