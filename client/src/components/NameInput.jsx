331540445565c3080de15d140dd5442aac329531
albin-karlsson <55614148+albin-karlsson@users.noreply.github.com>
2024-03-21 21:46:37 +0100

    Name input component

--- client/src/components/NameInput.jsx
+++ client/src/components/NameInput.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 function NameInput() {
+  const [name, setName] = useState("");
+  const [isNameMissing, setIsNameMissing] = useState(false);
+
+  function handleChange(e) {
+    const inputValue = e.target.value;
+    // Immediately transform the first character to uppercase if it's a letter
+    const updatedValue =
+      inputValue.length === 1
+        ? inputValue.toUpperCase()
+        : inputValue.charAt(0) + inputValue.slice(1);
+
+    setName(updatedValue);
+    if (updatedValue) {
+      setIsNameMissing(false);
+    }
+  }
+
+  function enterCouncil() {
+    if (name) {
+      console.log("Entering council...");
+      setIsNameMissing(false);
+    } else {
+      console.log("No name...");
+      setIsNameMissing(true);
+    }
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      enterCouncil();
+    }
+  }
+
   return (
     <div>
       <h3 className="sub-sub-header">please type your name to enter:</h3>
@@ -10,13 +43,20 @@ function NameInput() {
         <input
           className="name-input"
           type="text"
+          value={name}
           placeholder="your name"
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
         />
         <FontAwesomeIcon
           icon={faArrowRight}
           className="input-arrow"
+          onClick={enterCouncil}
         />
       </div>
+      <h3 className={`sub-sub-header ${!isNameMissing ? "hidden" : ""}`}>
+        please enter your name to proceed
+      </h3>
     </div>
   );
 }
