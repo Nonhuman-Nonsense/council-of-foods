2449f9e2ccb0f21fee7cafb3b44e47b2791e2bc2
albin-karlsson <55614148+albin-karlsson@users.noreply.github.com>
2024-04-20 10:29:26 +0200

    Refactor Output and TextOutput

--- client/src/components/TextOutput.jsx
+++ client/src/components/TextOutput.jsx
@@ -1,58 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-function TextOutput({ conversation }) {
-  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
-  const [currentSnippetIndex, setCurrentSnippetIndex] = useState(0);
-  const [currentMessageTextSnippet, setCurrentMessageTextSnippet] =
-    useState("");
-  const [initialize, setInitialize] = useState(true); // Flag to control initialization
+function TextOutput({ currentMessageTextSnippet }) {
   const textOutputStyle = {
     fontFamily: "Arial, sans-serif",
     backgroundColor: "rgba(0,0,0,0.7)",
   };
 
-  // Function to calculate the display time based on text length
-  const calculateDisplayTime = (text) => Math.max(3, text.length * 0.05);
-
-  useEffect(() => {
-    if (initialize && conversation.length > 0) {
-      setCurrentMessageIndex(0);
-      setCurrentSnippetIndex(0);
-      setInitialize(false); // Reset initialization flag after first setup
-    }
-  }, [conversation, initialize]);
-
-  useEffect(() => {
-    const processSnippets = () => {
-      if (conversation.length > currentMessageIndex) {
-        const snippets =
-          conversation[currentMessageIndex].text.split(/(?<=\.\s)/);
-        if (snippets.length > currentSnippetIndex) {
-          setCurrentMessageTextSnippet(snippets[currentSnippetIndex]);
-          const timeout = setTimeout(() => {
-            if (currentSnippetIndex < snippets.length - 1) {
-              setCurrentSnippetIndex(currentSnippetIndex + 1);
-            } else if (currentMessageIndex < conversation.length - 1) {
-              setCurrentMessageIndex(currentMessageIndex + 1);
-              setCurrentSnippetIndex(0);
-            }
-          }, calculateDisplayTime(snippets[currentSnippetIndex]) * 1000);
-          return () => clearTimeout(timeout);
-        }
-      }
-    };
-
-    processSnippets();
-  }, [currentMessageIndex, currentSnippetIndex, conversation]);
-
   return (
     <div style={{ textAlign: "center", width: "75%" }}>
-      <h2 style={textOutputStyle}>
-        Speaking:{" "}
-        {conversation.length > 0
-          ? conversation[currentMessageIndex].speaker
-          : ""}
-      </h2>
       <h2 style={textOutputStyle}>{currentMessageTextSnippet || ""}</h2>
     </div>
   );
