30260f9203663cc6f41c83001b97f9d0c7bd8b4d
Leo Fidjeland <leo.fidjeland@gmail.com>
2024-04-29 20:34:55 +0530

    strange comment error

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -8,15 +8,17 @@ function Loading() {
       objectFit: "cover"
   };
 
+
+  // TODO: safari version?
+  // <source
+  //   src={`/videos/loading.mp4`}
+  //   type={'video/mp4; codecs="hvc1"'} />
+
   return (
     <video style={styles} autoPlay loop muted playsInline>
-      //TODO: safari version?
-      // <source
-      //   src={`/videos/loading.mp4`}
-      //   type={'video/mp4; codecs="hvc1"'} />
       <source
-        src={`/videos/loading.webm`}
-        type={"video/webm"}/>
+          src={`/videos/loading.webm`}
+          type={"video/webm"} />
       </video>
   );
 }
