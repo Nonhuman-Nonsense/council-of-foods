9cb276b26e9d48289c66f8193faf551d8c1d04f1
Leo Fidjeland <leo.fidjeland@gmail.com>
2024-05-07 19:36:29 +0530

    Update Welcome.jsx

--- client/src/components/settings/Welcome.jsx
+++ client/src/components/settings/Welcome.jsx
@@ -1,29 +1,18 @@
 import React from "react";
 function Welcome({ humanName, onContinueForward }) {
+
+  const wrapper = {
+    maxWidth: "530px"
+  };
   return (
-      <div>
+      <div style={wrapper}>
         <div>
-          <h4>Dear {humanName},</h4>
+          <h3>Dear {humanName},</h3>
           <p>
-            Welcome to the Council of Foods! Here you can listen to foods
-            <br /> discussing the broken food system, and even take part in the
-            <br /> conversation. You will hear, from the foods themselves, what
-            <br /> their eco-social influence and guiding ethical principles are
-            and
-            <br /> discuss together what actions need to be taken to form a
-            locally
-            <br /> and globally sustainable food system.
+            Welcome to the Council of Foods! Here you can listen to foods discussing the broken food system, and even take part in the conversation. You will hear, from the foods themselves, what their eco-social influence and guiding ethical principles are and discuss together what actions need to be taken to form a locally and globally sustainable food system.
           </p>
           <p>
-            Each food has a different background the mass produced, the
-            <br /> locally grown, the genetically modified, the processed, the
-            fair
-            <br /> trade, the affordable and the biological. All the foods are
-            <br /> assigned an AI language model, each prompted on different
-            <br /> knowledges and ethical guidelines. Therefore, the members
-            have
-            <br /> divergent ethical positions and agendas, voicing a variety of
-            <br /> perspectives in the Council of Foods.
+            Each food has a different background the mass produced, the locally grown, the genetically modified, the processed, the fair trade, the affordable and the biological. All the foods are assigned an AI language model, each prompted on different knowledges and ethical guidelines. Therefore, the members have divergent ethical positions and agendas, voicing a variety of perspectives in the Council of Foods.
           </p>
         </div>
         <br/>
