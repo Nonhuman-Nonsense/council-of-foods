2a74c643d296d531aac8a669eb771c711b6d4494
Leo Fidjeland <leo.fidjeland@gmail.com>
2024-05-08 12:20:40 +0530

    lottie animations

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,25 +1,12 @@
 import React from "react";
+import Animation from "./Animation"
 
 function Loading() {
 
-  const styles = {
-      position: "absolute",
-      top: "73vh",
-      objectFit: "cover"
-  };
-
-
-  // TODO: safari version?
-  // <source
-  //   src={`/videos/loading.mp4`}
-  //   type={'video/mp4; codecs="hvc1"'} />
-
   return (
-    <video style={styles} autoPlay loop muted playsInline>
-      <source
-          src={`/videos/loading.webm`}
-          type={"video/webm"} />
-      </video>
+    <div style={{position: "absolute", top: "73vh"}}>
+      <Animation src={'/animations/loading.json'} style={{height: "150px"}} />
+    </div>
   );
 }
 
