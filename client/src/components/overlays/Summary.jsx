75488ddbb878235b01c83ce7c036f151b3459c7c
albin-karlsson <55614148+albin-karlsson@users.noreply.github.com>
2024-05-31 10:10:41 +0200

    Add useCreatePdf hook

--- client/src/components/overlays/Summary.jsx
+++ client/src/components/overlays/Summary.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { useMobile } from "../../utils";
+import useCreatePdf from "../../hooks/useCreatePdf";
 
 function Summary({ summary, meetingId }) {
-
   const isMobile = useMobile();
+  const { createPdf } = useCreatePdf();
 
   const wrapper = {
-    height: isMobile ? "calc(100vh - 55px)" : "calc(100vh - 60px - 56px - 20px)",
+    height: isMobile
+      ? "calc(100vh - 55px)"
+      : "calc(100vh - 60px - 56px - 20px)",
     marginBottom: isMobile ? "45px" : "56px",
     marginTop: !isMobile && "20px",
     width: "600px",
-    overflowY:"auto"
+    overflowY: "auto",
   };
 
   const protocol = {
@@ -19,12 +22,16 @@ function Summary({ summary, meetingId }) {
   };
 
   return (
-    <div style={wrapper} className="scroll">
+    <div
+      style={wrapper}
+      className="scroll"
+    >
       <h2>COUNCIL OF FOODS</h2>
+      <button onClick={() => createPdf(summary.text, meetingId)}>
+        Download PDF
+      </button>
       <h3>Meeting #{meetingId}</h3>
-      <div style={protocol}>
-        {summary.text}
-      </div>
+      <div style={protocol}>{summary.text}</div>
     </div>
   );
 }
