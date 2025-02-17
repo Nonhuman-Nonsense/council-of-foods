5c40e3e120355965531ede9b66070ad2977dd18c
Leo Fidjeland <leo.fidjeland@gmail.com>
2025-02-17 19:26:48 +0530

    experiment draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import Main from "./components/Main";
-// import About from "./components/overlays/About";
-// import Contact from "./components/overlays/Contact";
 
 function App() {
+
+  useEffect(() => {
+    const onResize = (event) => console.info("resize", event);
+
+    window.visualViewport.addEventListener('resize', onResize);
+
+    return () => {
+      window.visualViewport.removeEventListener('resize', onResize);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Main />
+        </BrowserRouter>
+      </div>
+      <div style={{ height: "300px" }}>hi</div>
+    </>
   );
 }
 
