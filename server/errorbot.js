6b9745d5d905a91ca31be9e8182411c22c005df8
Leo Fidjeland <leo.fidjeland@gmail.com>
2025-06-01 15:50:51 +0530

    error logs

--- server/errorbot.js
+++ server/errorbot.js
@@ -1,9 +1,14 @@
 if (process.env.COUNCIL_ERRORBOT) {
     console.log(`[init] will attempt to post errors to errorbot on ${process.env.COUNCIL_ERRORBOT}`);
+}else{
+    console.log(`[init] COUNCIL_ERRORBOT not set, will not report errors.`);
 }
 
 export async function reportError(err) {
-    if (!process.env.COUNCIL_ERRORBOT) return;
+    if (!process.env.COUNCIL_ERRORBOT){ 
+        console.log(`[Error] COUNCIL_ERRORBOT not set, will not report errors.`);
+        return;
+    }
 
     const sendStr = JSON.stringify({
         service: process.env.COUNCIL_DB_PREFIX,
@@ -18,15 +23,18 @@ export async function reportError(err) {
     }).catch(error => {
         //We better catch errors posting to the bot, otherwise we get endless loop
         console.error(error);
-        console.log('[Shutdown] Uncaught Exception, posting error failed.');
+        console.log('[Error] posting error FAIL');
+        console.log('[Shutdown] Uncaught Exception');
         process.exit(1);
     });
+    
+    console.log('[Error] posting error OK');
 }
 
 //For all unrecoverable errors, post the message to error bot, and then crash
 process.on('uncaughtException', async (err) => {
     console.error(err);
     await reportError(err);
-    console.log('[Shutdown] Uncaught Exception, posting error OK.');
+    console.log('[Shutdown] Uncaught Exception');
     process.exit(1);
 });
