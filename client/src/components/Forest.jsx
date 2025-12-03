3ad3a8193e6ea7ec49934be963f547b5d3d53438
Leo Fidjeland <leo.fidjeland@gmail.com>
2025-12-03 20:59:51 +0530

    volume adjustments

--- client/src/components/Forest.jsx
+++ client/src/components/Forest.jsx
@@ -25,19 +25,19 @@ function Forest({ currentSpeakerId, isPaused, audioContext }) {
     }, []); // Empty dependency array ensures this effect runs only once on mount and unmount
 
     const characters = [//Ratio is video width / height
-        { ref: useRef(null), id: "salmon", type: "transparent", audio: 0.3, zoom: 60, height: 9, left: 2.5, bottom: 42.5, ratio: 934 / 450 },
+        { ref: useRef(null), id: "salmon", type: "transparent", audio: 0.2, zoom: 60, height: 9, left: 2.5, bottom: 42.5, ratio: 934 / 450 },
         { ref: useRef(null), id: "bird", type: "transparent", height: 14, left: 12.5, bottom: 52, ratio: 708 / 612 },
-        { ref: useRef(null), id: "bumblebee", type: "transparent", audio: 0.3, zoom: 60, height: 10, left: -48, bottom: 44, ratio: 786 / 646 },
+        { ref: useRef(null), id: "bumblebee", type: "transparent", audio: 0.2, zoom: 60, height: 10, left: -48, bottom: 44, ratio: 786 / 646 },
         { ref: useRef(null), id: "concortapine", type: "image", height: 27, left: -73, bottom: 13, ratio: 724 / 918 },
-        { ref: useRef(null), id: "pine", type: "transparent", audio: 0.3, zoom: 100, height: 61, left: 26, bottom: 0, ratio: 1104 / 1920 },
-        { ref: useRef(null), id: "reindeer", type: "transparent", audio: 0.3, height: 16, left: -26.5, bottom: 27, ratio: 1040 / 956 },
-        { ref: useRef(null), id: "windturbine", type: "transparent", audio: 0.3, height: 22, left: 1, bottom: 69.5, ratio: 1066 / 946 },
-        { ref: useRef(null), id: "treeharvester", type: "transparent", audio: 0.3, zoom: 80, height: 19, left: 57.5, bottom: 1, ratio: 674 / 900 },
+        { ref: useRef(null), id: "pine", type: "transparent", audio: 0.2, zoom: 100, height: 61, left: 26, bottom: 0, ratio: 1104 / 1920 },
+        { ref: useRef(null), id: "reindeer", type: "transparent", audio: 0.2, height: 16, left: -26.5, bottom: 27, ratio: 1040 / 956 },
+        { ref: useRef(null), id: "windturbine", type: "transparent", audio: 0.2, height: 22, left: 1, bottom: 69.5, ratio: 1066 / 946 },
+        { ref: useRef(null), id: "treeharvester", type: "transparent", audio: 0.05, zoom: 80, height: 19, left: 57.5, bottom: 1, ratio: 674 / 900 },
         { ref: useRef(null), id: "kota", type: "transparent", always_on: true, height: 35, left: 54, bottom: 20, ratio: 574 / 1000 },
-        { ref: useRef(null), id: "lichen", type: "transparent", audio: 0.3, height: 25, left: 40, bottom: 53.5, ratio: 1332 / 1000 },
+        { ref: useRef(null), id: "lichen", type: "transparent", audio: 0.2, height: 25, left: 40, bottom: 53.5, ratio: 1332 / 1000 },
         { ref: useRef(null), id: "burningpine", type: "transparent", always_on: true, height: 14, left: -88, bottom: 85.5, ratio: 474 / 474 },
         { ref: useRef(null), id: "aurora", type: "transparent", always_on: true, height: 21, left: -35, bottom: 80, ratio: 1600 / 800 },
-        { ref: useRef(null), id: "mountain", type: "transparent", audio: 0.3, zoom: 40, height: 16, left: -52, bottom: 71.5, ratio: 1600 / 480 },
+        { ref: useRef(null), id: "mountain", type: "transparent", audio: 0.2, zoom: 40, height: 16, left: -52, bottom: 71.5, ratio: 1600 / 480 },
         { ref: useRef(null), id: "kota2", type: "image", height: 11, left: -19.5, bottom: 64, ratio: 564 / 400 },
         { ref: useRef(null), id: "snowyspruce", type: "image", height: 36, left: -37, bottom: 44.5, ratio: 1044 / 1800 },
     ];
@@ -128,7 +128,7 @@ function Forest({ currentSpeakerId, isPaused, audioContext }) {
             <img style={{ zIndex: "-5", height: "100%", position: "absolute", bottom: 0 }} src={`/backgrounds/forest${isMobile ? "-small" : ""}.avif`} alt="" />
             <div style={{ zIndex: "-4", height: "75.5%", position: "absolute", bottom: 0, left: "calc(50% - max(49dvh,147px))" }}>
                 <FoodAnimation type="transparent" character={{ id: "river" }} isPaused={isPaused} always_on={true} />
-                <BeingAudio id={'river'} volume={0.5} currentSpeakerId={currentSpeakerId} audioContext={audioContext} />
+                <BeingAudio id={'river'} volume={0.15} currentSpeakerId={currentSpeakerId} audioContext={audioContext} />
             </div>
             {characters.map((character, index) => (
                 <div key={index}>
@@ -214,7 +214,7 @@ function AmbientAudio({ audioContext }) {
     const sourceNode = useRef(null);
 
     //Global ambience volume
-    const onVolume = 0.3;
+    const onVolume = 0.05;
 
     const isDocumentVisible = useDocumentVisibility();
 
