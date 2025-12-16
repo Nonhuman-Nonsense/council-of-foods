b4770f1ee798bdfdf9ddd140267b3a7c76395340
Leo Fidjeland <leo.fidjeland@gmail.com>
2025-12-16 13:03:00 +0530

    app and index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import './i18n';
 import { BrowserRouter } from "react-router";
 import Main from "./components/Main";
 
-function App() {
+function App(): React.ReactElement {
   return (
     <div className="App">
       <BrowserRouter>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+if (!container) throw new Error("Failed to find the root element");
+const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
